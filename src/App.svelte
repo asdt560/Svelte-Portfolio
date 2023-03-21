1ec8a114422b2eabd1a5c01ba9a8d97293152bcd
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-03-21 19:43:34 -0300

    Initialize application

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>Justo Rago</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
+<style>
+</style>
